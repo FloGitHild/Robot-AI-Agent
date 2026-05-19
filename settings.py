e2cffd8dcf76d77f8b961b00823f5154f080d7c8
Florian Hildebrandt <florian-hildebrandt@gmx.net>
2026-05-19 15:57:53 +0200

    New Start

--- settings.py
+++
@@ -1,5 +0,0 @@
-local_llm_model = "qwen3.5:latest" # "phi3:latest", "mistral:latest  ", "qwen3.5:latest"
-KNOWN_FACES_DIR = "face_database"
-OBJECT_DETECTION_MODEL = "yolov8n.pt" # leichtes Modell, (n,s,m,l,x)
-CURRENT_HEARTBEAT = 2 # Sekunden zwischen den Scans, wird angepasst je nach modus
-CURRENT_MODE = "AUTO" # AUTO, PLAY, ASSIST, EXPLORE, IDLE
